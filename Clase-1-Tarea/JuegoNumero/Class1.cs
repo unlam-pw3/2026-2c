ca62b3809b1f63a2e7d0b644c633ed13ff4de8b1
giancroci <giancroci5@gmail.com>
2026-04-07 13:17:27 -0300

    Lógica del juego de adivinación de numeros y pruebas unitarias finalizadas

---
+++ Clase-1-Tarea/JuegoNumero/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace JuegoNumero
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Clase-1-Tarea/JuegoNumero/Logica/INumero.cs
@@ -0,0 +1,12 @@
+﻿
+namespace JuegoNumero.Logica
+{
+    public interface INumero
+    {
+        int Valor { get; }
+        int Intentos { get; }
+        bool IsGanado { get; }
+
+        string EnviarIntento(int intento);
+    }
+}
