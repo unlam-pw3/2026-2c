18140ba3fc56eb33a8bcb1fa31c86b341b55b41b
TomasZ-png <animacionesc4d@gmail.com>
2026-04-16 16:06:02 -0300

    Tarea de adivinanza Thomas Murua

---
+++ Clase1/Clase-1-Tarea-juego-adivinanza/Juego/IJuegoAdivinanza.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase_1_Tarea_juego_adivinanza.Juego
+{
+    public interface IJuegoAdivinanza
+    {
+        public string AdivinarNumero(string entrada);
+        public bool JuegoGanado { get; }
+        public int IntentosRealizados { get; }
+    }
+}
